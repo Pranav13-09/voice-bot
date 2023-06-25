a1a2beceaf225c6795b8f2af74d530b6ec10e892
Pranav13-09 <morepranav51@gmail.com>
2023-06-25 21:39:27 +0530

    plz hoja

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
