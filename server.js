d8cbc0a7f92a0d65cba748e8915904907dbe1809
Pranav13-09 <morepranav51@gmail.com>
2023-06-26 00:33:17 +0530

    hello

--- server.js
+++ server.js
@@ -65,3 +65,9 @@ server.listen(port, () => {
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+app.get("/test", (req, res) => {
+  console.log("i am here okk");
+  console.log(io, "i am here");
+  return res.status(200).json({ message: "Okk" });
+});
