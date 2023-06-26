6396e01ea399481c65850261e6297861a2bb28b2
Pranav13-09 <morepranav51@gmail.com>
2023-06-26 12:35:56 +0530

    Microphone permission added

--- public/main.js
+++ public/main.js
@@ -12,82 +12,106 @@ const conversationContainer = document.getElementById("conversationContainer");
 transcript.textContent =
   "Please click on Start Listening  button to  ask question to the ChatBot";
 
-// Adding the event listener on button to start recording the  queries of user
-startButton.addEventListener("click", () => {
-  recognition = new webkitSpeechRecognition(); // Builtin WebSpeechApi  to convert the voice into text
-  startButton.disabled = true;
-  transcript.textContent = "Listening...";
-  recognition.continuous = true;
-  recognition.interimResults = true;
-  recognition.lang = "en-US";
-  recognition.start();
+// Check if the browser supports the getUserMedia API
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  // Check microphone permission status
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then(function (stream) {
+      // Microphone permission granted, proceed with chatbot functionality
+      // Add code to start the chatbot or enable the "Start Listening" button
+      startButton.addEventListener("click", () => {
+        recognition = new webkitSpeechRecognition(); // Builtin WebSpeechApi  to convert the voice into text
+        startButton.disabled = true;
+        transcript.textContent = "Listening...";
+        recognition.continuous = true;
+        recognition.interimResults = true;
+        recognition.lang = "en-US";
+        recognition.start();
 
-  recognition.onstart = () => {
-    //To automatically dtermine the stop time and sending the query to the backend
-    silenceTimer = setTimeout(() => {
-      recognition.stop();
-    }, 2000); // Adjust the silence duration as needed (in milliseconds)
-  };
+        recognition.onstart = () => {
+          //To automatically dtermine the stop time and sending the query to the backend
+          silenceTimer = setTimeout(() => {
+            recognition.stop();
+          }, 2000); // Adjust the silence duration as needed (in milliseconds)
+        };
 
-  recognition.onresult = (event) => {
-    const result = event.results[event.results.length - 1]; //converting the voice into text
-    const text = result[0].transcript;
-    transcript.innerHTML = text;
+        recognition.onresult = (event) => {
+          const result = event.results[event.results.length - 1]; //converting the voice into text
+          const text = result[0].transcript;
+          transcript.innerHTML = text;
 
-    clearTimeout(silenceTimer);
-    silenceTimer = setTimeout(() => {
-      recognition.stop();
-      console.log("Before emitting the event");
-      socket.emit("user-query", text); //emitting the event "user query which is listened on the backend , sending the  user query in foem of text to the backend server"
-      console.log("event is emitted");
-      loadingSpinner.style.display = "block"; //Loading the spinner while request is being processed
-    }, 2000); // Adjust the silence duration as needed (in milliseconds)
-  };
-});
+          clearTimeout(silenceTimer);
+          silenceTimer = setTimeout(() => {
+            recognition.stop();
+            console.log("Before emitting the event");
+            socket.emit("user-query", text); //emitting the event "user query which is listened on the backend , sending the  user query in foem of text to the backend server"
+            console.log("event is emitted");
+            loadingSpinner.style.display = "block"; //Loading the spinner while request is being processed
+          }, 2000); // Adjust the silence duration as needed (in milliseconds)
+        };
+      });
 
-// Adding the event listener to stop recording the user voice
-stopButton.addEventListener("click", () => {
-  if (recognition) {
-    clearTimeout(silenceTimer);
-    recognition.stop();
-    recognition = null;
-    startButton.disabled = false;
-    loadingSpinner.style.display = "none";
-    transcript.textContent =
-      "Please click on Start Listening  button to  ask question to the ChatBot";
-  }
-});
+      // Adding the event listener to stop recording the user voice
+      stopButton.addEventListener("click", () => {
+        if (recognition) {
+          clearTimeout(silenceTimer);
+          recognition.stop();
+          recognition = null;
+          startButton.disabled = false;
+          loadingSpinner.style.display = "none";
+          transcript.textContent =
+            "Please click on Start Listening  button to  ask question to the ChatBot";
+        }
+      });
 
-function createAudioPlayer(base64Data) {
-  // Dynamically creating the  audio player and user query based on the base64 encoded audio data received from tjhe backend server
-  const audioContainer = document.createElement("div");
-  audioContainer.classList.add("queryItem");
+      function createAudioPlayer(base64Data) {
+        // Dynamically creating the  audio player and user query based on the base64 encoded audio data received from tjhe backend server
+        const audioContainer = document.createElement("div");
+        audioContainer.classList.add("queryItem");
 
-  //Adding dynamic styles
-  const transcriptText = transcript.textContent.trim();
-  const transcriptElement = document.createElement("p");
-  transcriptElement.textContent = transcriptText;
+        //Adding dynamic styles
+        const transcriptText = transcript.textContent.trim();
+        const transcriptElement = document.createElement("p");
+        transcriptElement.textContent = transcriptText;
 
-  //Creating audio player with help of audio tag in html
-  const audioElement = document.createElement("audio");
+        //Creating audio player with help of audio tag in html
+        const audioElement = document.createElement("audio");
 
-  //Assigning the src as the base64 encoded data
-  audioElement.src = "data:audio/mpeg;base64," + base64Data;
-  audioElement.controls = true;
-  audioElement.autoplay = true; // Autoplay
-  console.log(audioElement, "ia m audio Element");
+        //Assigning the src as the base64 encoded data
+        audioElement.src = "data:audio/mpeg;base64," + base64Data;
+        audioElement.controls = true;
+        audioElement.autoplay = true; // Autoplay
+        console.log(audioElement, "ia m audio Element");
 
-  audioContainer.appendChild(transcriptElement);
-  audioContainer.appendChild(audioElement);
-  conversationContainer.appendChild(audioContainer);
+        audioContainer.appendChild(transcriptElement);
+        audioContainer.appendChild(audioElement);
+        conversationContainer.appendChild(audioContainer);
+      }
+
+      //Listening to the event emiited from the backend server
+      socket.on("bot-response", (base64Data) => {
+        // Create the audio player with the base64-encoded audio data, converting the base64 encoded audio data to the audio player(text to speech)
+        createAudioPlayer(base64Data);
+        loadingSpinner.style.display = "none";
+        startButton.disabled = false;
+        transcript.textContent =
+          "Please click on Start Listening  button to  ask question to the ChatBot";
+      });
+    })
+    .catch(function (error) {
+      // Microphone permission denied or not supported
+      // Display an alert to the user
+      alert(
+        "Microphone permission is required to use the chatbot. Please grant microphone permission to begin."
+      );
+    });
+} else {
+  // getUserMedia API not supported by the browser
+  // Display an alert or fallback message to inform the user
+  alert(
+    "Your browser does not support the required features to use the chatbot. Please use a modern browser with microphone support."
+  );
 }
 
-//Listening to the event emiited from the backend server
-socket.on("bot-response", (base64Data) => {
-  // Create the audio player with the base64-encoded audio data, converting the base64 encoded audio data to the audio player(text to speech)
-  createAudioPlayer(base64Data);
-  loadingSpinner.style.display = "none";
-  startButton.disabled = false;
-  transcript.textContent =
-    "Please click on Start Listening  button to  ask question to the ChatBot";
-});
+// Adding the event listener on button to start recording the  queries of user
